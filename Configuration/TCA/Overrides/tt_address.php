f1fabbe905a08c05b510559156cd3f56368e44e5
thomasruta <thomasruta.us@gmail.com>
2018-03-04 17:25:12 +0100

    address integration start

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -1,12 +1,12 @@
 <?php
 defined('TYPO3_MODE') || die();
-
+/*
 if (!isset($GLOBALS['TCA']['tt_address']['ctrl']['type'])) {
     if (file_exists($GLOBALS['TCA']['tt_address']['ctrl']['dynamicConfigFile'])) {
         require_once($GLOBALS['TCA']['tt_address']['ctrl']['dynamicConfigFile']);
     }
     // no type field defined, so we define it here. This will only happen the first time the extension is installed!!
-    $GLOBALS['TCA']['tt_address']['ctrl']['type'] = 'tx_extbase_type';
+  //  $GLOBALS['TCA']['tt_address']['ctrl']['type'] = 'tx_extbase_type';
     $tempColumnstx_tp3jobs_tt_address = [];
     $tempColumnstx_tp3jobs_tt_address[$GLOBALS['TCA']['tt_address']['ctrl']['type']] = [
         'exclude' => true,
@@ -31,9 +31,10 @@ if (!isset($GLOBALS['TCA']['tt_address']['ctrl']['type'])) {
     '',
     'after:' . $GLOBALS['TCA']['tt_address']['ctrl']['label']
 );
+*/
 
 /* inherit and extend the show items from the parent class */
-
+/*
 if (isset($GLOBALS['TCA']['tt_address']['types']['0']['showitem'])) {
     $GLOBALS['TCA']['tt_address']['types']['Tx_Tp3Jobs_Company']['showitem'] = $GLOBALS['TCA']['tt_address']['types']['0']['showitem'];
 } elseif(is_array($GLOBALS['TCA']['tt_address']['types'])) {
@@ -52,3 +53,4 @@ $GLOBALS['TCA']['tt_address']['columns'][$GLOBALS['TCA']['tt_address']['ctrl']['
     '',
     'EXT:/Resources/Private/Language/locallang_csh_.xlf'
 );
+*/
