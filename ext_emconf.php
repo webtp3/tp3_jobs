c3f7a1c4a933fe4d5b0be0a1b5384ebd2a6fc4cd
tudy <email@thomasruta.de>
2019-02-20 02:16:30 -0800

    [fix] version 1.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '0.2.0',
+  'version' => '1.2.0',
   'author_company' => 'tp3',
   'constraints' =>
   [
